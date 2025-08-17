9693801767818e48578c0f2153b33fbc33a89d31
O-21-3 <sasami@st.rim.or.jp>
2025-08-17 22:02:06 +0900

    Update Fate Farming(JP Client).lua

--- SND New/Fate Farming/Fate Farming(JP Client).lua
+++ SND New/Fate Farming/Fate Farming(JP Client).lua
@@ -1553,7 +1553,7 @@ end
 function AcceptNPCFateOrRejectOtherYesno()
     if Addons.GetAddon("SelectYesno").Ready then
         local dialogBox = GetNodeText("SelectYesno", 1, 2)
-        if type(dialogBox) == "string" and dialogBox:find("The recommended level for this FATE is") then
+        if type(dialogBox) == "string" and dialogBox:find("このF.A.T.E.の推奨レベルは") then
             Engines.Run("/callback SelectYesno true 0") --accept fate
         else
             Engines.Run("/callback SelectYesno true 1") --decline all other boxes
